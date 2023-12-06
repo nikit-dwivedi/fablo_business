87266b5aa30e06a8cf98bd9ee570c25cac6f5604
nikit dwivedi <100762712+nikit-dwivedi@users.noreply.github.com>
2023-12-06 17:31:46 +0530

    Update oneSignal.service.js

--- app/src/api/v1/services/oneSignal.service.js
+++ app/src/api/v1/services/oneSignal.service.js
@@ -4,7 +4,7 @@ const { getUserNotificationId } = require("../services/firebase.service");
 async function sendNotification(userIDArray, messageDetails) {
     const headers = {
         "Content-Type": "application/json; charset=utf-8",
-        "Authorization": "Basic NjQ1MzdmMzYtYmJmZS00ZjQwLWIxMzgtMzQ2MzljNDZkY2Fh"
+        "Authorization": ""
     };
     const data = {
         app_id: "1b4778ad-4a99-499e-be8c-33f3356161a8",
